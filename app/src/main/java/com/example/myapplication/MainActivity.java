ac9a68c127d6b86e01a45cf0380f7d91a892b2f6
gualagugu <jling6@illinois.edu>
2019-04-27 20:29:46 -0500

    a

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,12 +2,54 @@ package com.example.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button submit;
+    private Spinner select;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        addItemsOnSpinner();
+        addListenerOnButton();
     }
+
+    public void addItemsOnSpinner() {
+        select = (Spinner) findViewById(R.id.spinner);
+        List<String> list = new ArrayList<String>();
+        list.add("list 1");
+        list.add("list 2");
+        list.add("list 3");
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, list);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        select.setAdapter(dataAdapter);
+    }
+    public void addListenerOnButton() {
+        select = (Spinner) findViewById(R.id.spinner);
+        submit = (Button) findViewById(R.id.submit);
+
+        submit.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(MainActivity.this,
+                        "OnClickListener : " +
+                                "\nSpinner: "+ String.valueOf(select.getSelectedItem()),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
 }
